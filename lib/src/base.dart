b45f36d56aa5cd049f4f131fac99eae15c3919ea
WingCH <wingch.apps@gmail.com>
2022-01-04 12:50:09 +0800

    setup http client

--- lib/src/base.dart
+++ lib/src/base.dart
@@ -1,3 +1,11 @@
+import 'package:http/http.dart' as http;
+
 class IcookExplode {
   int addOne(int value) => value + 1;
+
+  Future<String> tryHttp() async {
+    var url = Uri.parse('https://icook.tw/search/羅宋湯/');
+    var response = await http.get(url);
+    return response.body;
+  }
 }
--- test/icook_explode_test.dart
+++ test/icook_explode_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:icook_explode/icook_explode.dart';
 
 void main() {
@@ -8,5 +7,13 @@ void main() {
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
+  });
+
+  test('try http', () async {
+    final calculator = IcookExplode();
+
+    var response = await calculator.tryHttp();
+
+    expect(response.isEmpty, false);
   });
 }
