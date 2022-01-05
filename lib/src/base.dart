9a06e0c3ad5c68817c47ea6d7106b67f19a991ac
WingCH <wingch.apps@gmail.com>
2022-01-05 13:33:51 +0800

    update parser

--- lib/src/base.dart
+++ lib/src/base.dart
@@ -14,9 +14,13 @@ class IcookExplode {
   String? tryParser(String rawHtml) {
     HtmlDocument document = parseHtmlDocument(rawHtml);
 
+    /// 食譜名稱
+    /// e.g: 羅宋湯
     final nameNode = document.getElementsByClassName("browse-title-name");
     String? name = nameNode.isEmpty ? null : nameNode.first.text;
 
+    // 食譜簡介
+    // e.g: description
     final descriptionElement = document
         .getElementsByClassName("styles-module__searchKeywordContent___hdMIz");
     String? description = descriptionElement.isEmpty
@@ -25,6 +29,13 @@ class IcookExplode {
             ? null
             : descriptionElement.first.nodes.first.text?.trim();
 
+    /// 食譜常見料理
+    /// e.g: [
+    //         "羅宋牛肉湯",
+    //         "蔬菜湯",
+    //         "牛肉湯",
+    //         "番茄湯"
+    //     ]
     final suggestionsElement = document.querySelectorAll(
         "#o-wrapper > div:nth-child(6) > div.row.row--flex > main > header > section:nth-child(5) > ul > li > a");
     final List<String>? suggestions = suggestionsElement
@@ -33,10 +44,53 @@ class IcookExplode {
         .whereType<String>()
         .toList();
 
-    final recipesElement = document.querySelectorAll(
-        "#o-wrapper > div:nth-child(6) > div.row.row--flex > main > ul.result-browse-layout.bottom-sticky-ad-waypoint--in > li.browse-recipe-item");
-    
-    // final List<String>? recipes = recipesElement.map((element) => element.)
-    return name;
+    //
+    final List<Node>? recipesNode =
+        document.getElementsByClassName("browse-recipe-item");
+
+    final tests = recipesNode?.map((recipeNode) {
+      final List<Node> childNode = recipeNode.childNodes
+          .where((element) => element.nodeType == 1)
+          .toList();
+
+      for (var node in childNode) {
+        final parent = node.parent;
+        // 內頁路徑, e.g: /recipes/397794
+        final String? detail_path =
+            parent?.querySelector("a")?.getAttribute("href");
+        // 圖片連結, e.g: https://imageproxy.icook.network/resize?background=255%2C255%2C255&amp;height=150&amp;nocrop=false&amp;stripmeta=true&amp;type=auto&amp;url=http%3A%2F%2Ftokyo-kitchen.icook.tw.s3.amazonaws.com%2Fuploads%2Frecipe%2Fcover%2F397794%2F72545b5990736c25.jpg&amp;width=200
+        final String? imgUrl = parent
+            ?.querySelector("a > article > div.browse-recipe-cover > img")
+            ?.getAttribute("data-src");
+
+        // e.g: 羅宋湯
+        final String? name = parent
+            ?.querySelector(
+                "a > article > div.browse-recipe-content > div > h2")
+            ?.text
+            ?.trim();
+        // e.g: 牛肉羅宋湯，一鍋到底的不正宗口味，哈哈！沒買到月桂葉，但是味道也是很美味。
+        final String? description = parent
+            ?.querySelector(
+                "a > article > div.browse-recipe-content > div > blockquote")
+            ?.text
+            ?.trim();
+        // 成份, e.g: 食材：牛肋條、牛番茄、鹽巴、紅蘿蔔、白胡椒粉、洋蔥、黑胡椒、義大利香料粉、番茄醬
+        final String? ingredient = parent
+            ?.querySelector("a > article > div.browse-recipe-content > div > p")
+            ?.text
+            ?.trim();
+        // 烹飪時間, e.g: 45 分
+        final String? cookingTime = parent
+            ?.querySelector(
+                "a > article > div.browse-recipe-content > ul.browse-recipe-meta > li.browse-recipe-meta-item:nth-child(1)")
+            ?.text
+            ?.trim();
+
+        print(ingredient);
+      }
+      return "a";
+    });
+    return tests.toString();
   }
 }
---
+++ lib/src/extension/list_ex.dart
@@ -0,0 +1,9 @@
+// https://stackoverflow.com/a/66385708/5588637
+extension MyIterable<T> on Iterable<T> {
+  T? get firstOrNull => isEmpty ? null : first;
+
+  T? firstWhereOrNull(bool Function(T element) test) {
+    final list = where(test);
+    return list.isEmpty ? null : list.first;
+  }
+}
